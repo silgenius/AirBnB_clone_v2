e01f4845ae8ce7eeaeb0f62f9627993c2e033c74
silgenius <olutademartin@gmail.com>
2024-06-24 18:24:52 -0700

    Refactor code

--- models/state.py
+++ models/state.py
@@ -17,11 +17,7 @@ class State(BaseModel, Base):
     __tablename__ = "states"
 
     name = Column(String(128), nullable=False)
-
-    storage_type = getenv('HBNB_TYPE_STORAGE')
-
-    if storage_type == "db":
-        cities = relationship("City", backref="state", cascade="all, delete-orphan")
+    cities = relationship("City", backref="state", cascade="all, delete-orphan")
     
     @property
     def cities(self):
