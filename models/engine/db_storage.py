762b37bf3ea2f114b8f1a24b11d98c1764b07078
silgenius <olutademartin@gmail.com>
2024-06-25 04:16:16 -0700

    Hot fix

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -7,8 +7,8 @@
     stoorage of data into database.
 """
 
-from sqlalchemy import create_engine, MetaData
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, scoped_session
 from os import getenv
 from models.city import City
 from models.state import State
@@ -16,24 +16,25 @@ from models.review import Review
 from models.place import Place
 from models.amenity import Amenity
 from models.user import User
+from models.base_model import Base
 
 
 class DBStorage:
     __engine = None
     __session = None
 
-    password = getenv('HBNB_MYSQL_PWD')
-    username = getenv('HBNB_MYSQL_USER')
-    database = getenv('HBNB_MYSQL_DB')
-    localhost = getenv('HBNB_MYSQL_HOST')
-    env = getenv('HBNB_ENV')
-
     def __init__(self):
-        self.__engine = create_engine(f'mysql+mymsqldb://{username}:\
-                {password}@{localhost}/{database}', pool_pre_ping=True)
+
+        password = getenv('HBNB_MYSQL_PWD')
+        username = getenv('HBNB_MYSQL_USER')
+        database = getenv('HBNB_MYSQL_DB')
+        host = getenv('HBNB_MYSQL_HOST')
+        env = getenv('HBNB_ENV')
+
+        self.__engine = create_engine(f'mysql+mysqldb://{username}:{password}@{host}/{database}', pool_pre_ping=True)
 
         if env == "test":
-            Base.metadata.drop_all(engine)
+            Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
         if cls:
@@ -43,17 +44,18 @@ class DBStorage:
             cls_list = [User, State, City, Amenity, Place, Review]
         new_dict = {}
         for cls in cls_list:
-            data = self.__session.query(cls)
+            results = self.__session.query(cls)
             for result in results:
+                print(result)
                 key = result[0].__class.__name + "." + result[0].id
                 new_dict[key] = result[0]
         return new_dict
 
     def new(self, obj):
-        self.__session.new(obj)
+        self.__session.add(obj)
 
     def save(self):
-        session.commit()
+        self.__session.commit()
 
     def delete(self, obj=None):
         if obj is not None:
@@ -61,6 +63,6 @@ class DBStorage:
             self.save()
 
     def reload(self):
-        Base.metadata.create_all(engine)
+        Base.metadata.create_all(self.__engine)
         Session = scoped_session(sessionmaker(bind=self.__engine, expire_on_commit=False))
         self.__session = Session()
