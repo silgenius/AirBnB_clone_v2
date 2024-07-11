4993782219fcdcec340a3ba8995ad2f91b027ef0
martin olutade <112440359+silgenius@users.noreply.github.com>
2024-07-12 00:27:14 +0100

    Update 100-clean_web_static.py

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -6,22 +6,28 @@ out-of-date archives, using the function do_clean"""
 import os
 from fabric.api import *
 
-env.hosts = ['54.88.64.221', '54.87.212.173']
 
+env.hosts = ['54.88.64.221', '54.87.212.173']
+env.user = 'ubuntu'
 
 def do_clean(number=0):
     """
     a Fabric script (based on the file 3-deploy_web_static.py) that deletes
     out-of-date archives, using the function do_clean:
     """  
-    number = 1 if int(number) == 0 else int(number)
-    archives = sorted(os.listdir("versions"))
-    [archives.pop() for i in range(number)]
+    if number < 1:
+        number = 1
+
+    # Clean local versions folder
     with lcd("versions"):
-        [local("rm ./{}".format(archive)) for archive in archives]
-    
+        local_archives = local("ls -1t", capture=True).split()
+        archives_to_delete = local_archives[number:]
+        for archive in archives_to_delete:
+            local("rm ./{}".format(archive))
+
+    # Clean remote releases folder
     with cd("/data/web_static/releases"):
-        archives = run("ls -tr").split()
-        archives = [archive for archive in archives if "web_static_" in archives]
-        [archives.pop() for i in range(number)]
-        [run("rm -rf ./{}".format(archive)) for archive in archives]
+        remote_archives = run("ls -1t").split()
+        archives_to_delete = remote_archives[number:]
+        for archive in archives_to_delete:
+            run("rm -rf ./{}".format(archive))
