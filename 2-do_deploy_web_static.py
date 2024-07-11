6bf13ec674166e8da5103f59551c6154089df138
martin olutade <112440359+silgenius@users.noreply.github.com>
2024-07-11 22:28:35 +0100

    Update 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -8,8 +8,9 @@
 from fabric.api import sudo, put, env
 import os
 
-
+env.user = 'ubuntu'env.user = 'ubuntu'
 env.hosts = ['54.88.64.221', '54.87.212.173']
+env.key_filename = "~/.ssh/id_rsa_alx"
 
 
 def do_deploy(archive_path):
