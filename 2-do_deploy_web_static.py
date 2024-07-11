d98add3f9d64bf458ea64b4bac5cb4584c2a843c
martin olutade <112440359+silgenius@users.noreply.github.com>
2024-07-11 23:04:24 +0100

    Update: Apply new approach 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,3 +1,9 @@
+#!/usr/bin/python3
+"""
+ a Fabric script (based on the file 1-pack_web_static.py) that distributes
+ an archive to your web servers, using the function do_deploy:
+ """
+
 from fabric.api import env, put, run
 import os
 
